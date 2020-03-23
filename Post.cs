808658993eac7ffe8bae9b94807da69e91d964b0
nabsabt <36499901+nabsabt@users.noreply.github.com>
2020-03-23 21:49:37 +0100

    Add files via upload

---
+++ Post.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace post
+{
+    class Post
+    {
+
+        private string Title;
+        private string User;
+
+        private int Vote;
+        private string PostField;
+        
+        public void SetTitle(string title)
+        {
+            Title = title;
+        }
+        public string GetTitle()
+        {
+            return Title;
+        }
+        ////
+        public void SetUser(string user)
+        {
+            User = user;
+        }
+        public string GetUser()
+        {
+            return User;
+        }
+        //////
+        public void SetVote(int vote)
+        {
+            Vote = vote;
+        }
+        public int GetVote()
+        {
+            return Vote;
+        }
+
+        ///////////
+        public void SetPostField(string postField)
+        {
+            PostField = postField;
+        }
+        public string GetPostField()
+        {
+            return PostField;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace post
+{
+    class Program
+    {
+
+
+        static Post postObj = new Post();
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type \"post\" if you want to post something, or type \"show posts\" if you want to see the posts!");
+            string userInput = Console.ReadLine();
+            if (userInput == "post")
+            {
+                PostMethod();
+            }
+            else if(userInput =="show posts")
+            {
+                if (postObj.GetPostField()=="")
+                {
+                    Console.WriteLine("No posts yet.");
+                }
+                else
+                {
+                    ShowPosts();
+                }
+            }
+            Console.WriteLine("Now type \"post\" if you want to post something again, or type \"show posts\" if you wanna see the posts!");
+            userInput = Console.ReadLine();
+            if (userInput == "post")
+            {
+                PostMethod();
+            }
+            else if (userInput == "show posts")
+            {
+                if (postObj.GetPostField() == "")
+                {
+                    Console.WriteLine("No posts yet.");
+                }
+                else
+                {
+                    ShowPosts();
+                }
+            }
+
+
+        }
+
+        public static void PostMethod()
+        {
+            Console.WriteLine("Hi! Before post, enter your name: ");
+            string name = Console.ReadLine();
+            postObj.SetUser(name);
+            Console.WriteLine("Now enter the title of the post: ");
+            string title = Console.ReadLine();
+            postObj.SetTitle(title);
+            Console.WriteLine("Now type in your post...");
+            string posttext = Console.ReadLine();
+            postObj.SetPostField(posttext);
+            Console.WriteLine("Your post is sent.");
+        }
+
+        public static void ShowPosts()
+        {
+            Console.WriteLine("Title: " + postObj.GetTitle());
+            Console.WriteLine("Sent by: " + postObj.GetUser());
+            Console.WriteLine("Post text: " + postObj.GetPostField());
+        }
+
+    }
+}
